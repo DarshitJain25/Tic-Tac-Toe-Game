60a1ca28ff6434973a95207044aaa82270aaff73
DarshitJain25 <darshitjain25oct@gmail.com>
2026-05-11 04:28:56 +0530

    Text changed in main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import Square from "./Elements.jsx";
 import Board from "./Board.jsx";
 
 function App() {
-  const [count, setCount] = React.useState(0);
-
   return (
     <>
       <h1>Tic-Tac-Toe start</h1>
